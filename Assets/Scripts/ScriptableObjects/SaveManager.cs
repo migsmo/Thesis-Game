221e45bf094f99c02e3d703636dffdfa80ed2788
Jolo Narvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-03-25 14:11:56 +0800

    added level performance logs

--- Assets/Scripts/ScriptableObjects/SaveManager.cs
+++ Assets/Scripts/ScriptableObjects/SaveManager.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using UnityEngine;
 
 namespace Resources
@@ -37,6 +38,31 @@ namespace Resources
             string data = File.ReadAllText(Application.streamingAssetsPath + Filename);
             temp = JsonUtility.FromJson<LevelData>(data);
             return temp.starsEarned;
+        }
+
+        public void generateLog(Report report){
+            string time = DateTime.Now.ToString("HH:mm:ss");
+            string fileName = DateTime.Now.Month + "-" + 
+                            DateTime.Now.Day + "-" + 
+                            DateTime.Now.Year + "-" + 
+                            time + "-" +
+                            "Level" + report.levelNumber;
+            string path = Application.dataPath + Directory + "/" + fileName;
+            
+            DateTime currentDateTime = DateTime.Now;
+            // Create a new file and write data to it
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(currentDateTime.ToString());
+                writer.WriteLine("Level " + report.levelNumber + " Performace Breakdown:");
+
+                for(int i = 0; i < report.exerciseList.Length; i++)
+                {
+                    writer.WriteLine(report.exerciseList[i] + " " + report.percentageList[i] + "%");
+                }
+                writer.WriteLine("Total Percentage: " + report.totalPercentage + "%");
+                writer.WriteLine("Total Stars Earned: " + report.earnedStars); 
+            }
         }
     }
 }
