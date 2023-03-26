34c3fdef805f05e4c1264d57df78c0f59b4d34c6
jolonarvaez <jose_wilfredo_narvaez@dlsu.edu.ph>
2023-03-26 11:44:02 +0800

    refactored SaveManager generate logs function

--- Assets/Scripts/ScriptableObjects/SaveManager.cs
+++ Assets/Scripts/ScriptableObjects/SaveManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 
 namespace Resources
@@ -7,7 +8,7 @@ namespace Resources
     [System.Serializable]
     public class SaveManager
     {
-        private string Directory = "/Resources/LevelData/";
+        private string Directory = "/LevelData/";
         private string Filename = "";
         public Level[] loadedLevels;
 
@@ -41,28 +42,28 @@ namespace Resources
         }
 
         public void generateLog(Report report){
-            string time = DateTime.Now.ToString("HH:mm:ss");
-            string fileName = DateTime.Now.Month + "-" + 
-                            DateTime.Now.Day + "-" + 
-                            DateTime.Now.Year + "-" + 
-                            time + "-" +
-                            "Level" + report.levelNumber + ".txt";
-            string path = Application.dataPath + Directory + "/" + fileName;
-            
-            DateTime currentDateTime = DateTime.Now;
-            // Create a new file and write data to it
-            using (StreamWriter writer = new StreamWriter(path))
-            {
-                writer.WriteLine(currentDateTime.ToString());
-                writer.WriteLine("Level " + report.levelNumber + " Performace Breakdown:");
+            string time = DateTime.Now.ToString("HH-mm-ss");
+            string fileName = DateTime.Now.Month + "-" +
+                              DateTime.Now.Day + "-" +
+                              DateTime.Now.Year + "-" +
+                              time + "-" +
+                              "Level" + report.levelNumber + ".txt";
+            string path = Application.streamingAssetsPath + Directory + fileName;
+            string data = "";
+            string newLine = "\n";
 
-                for(int i = 0; i < report.exerciseList.Length; i++)
-                {
-                    writer.WriteLine(report.exerciseList[i] + " " + report.percentageList[i] + "%");
-                }
-                writer.WriteLine("Total Percentage: " + report.totalPercentage + "%");
-                writer.WriteLine("Total Stars Earned: " + report.earnedStars); 
+            DateTime currentDateTime = DateTime.Now;
+            
+            File.AppendAllText(path, currentDateTime.ToString() + newLine);
+            File.AppendAllText(path, "Level " + report.levelNumber + " Performace Breakdown:" + newLine);
+
+            for (int i = 0; i < report.exerciseList.Length; i++)
+            {
+                File.AppendAllText(path, report.exerciseList[i] + " " + report.percentageList[i] + "%" + newLine);
             }
+            
+            File.AppendAllText(path, "Total Percentage: " + report.totalPercentage + "%");
+            File.AppendAllText(path, "Total Stars Earned: " + report.earnedStars);
         }
     }
 }
